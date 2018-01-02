aac640c636837d3814cb73d1bee513e4de12a5ab
petertbdtu <s175210@student.dtu.dk>
2018-01-02 12:17:10 +0100

    Initial commit.

---
+++ src/main/java/gr_34/CDIO_Final/App.java
@@ -0,0 +1,13 @@
+package gr_34.CDIO_Final;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
