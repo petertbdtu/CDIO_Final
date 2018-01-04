3463206b913ac00c700553382ac3aa5058592af2
Sebastian <s170423@student.dtu.dk>
2018-01-04 11:55:01 +0100

    filer åbenbart slettet fix

---
+++ src/main/java/gr_34/CDIO_Final/Main.java
@@ -0,0 +1,13 @@
+package gr_34.CDIO_Final;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
