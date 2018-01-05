ec135da26ed6a599dfaa1f5c5ccdaabe2ddc8003
Burhan27 <32563710+Burhan27@users.noreply.github.com>
2018-01-05 14:01:37 +0100

    rederi + bryggeri klassen + opdateret felt, gade og AbstraktEjendom

--- src/main/java/gr_34/entity/AbstraktEjendom.java
+++ src/main/java/gr_34/entity/AbstraktEjendom.java
@@ -1,21 +1,17 @@
 package gr_34.entity;
 
-import java.awt.Color;
-
 public abstract class AbstraktEjendom extends Felt {
 
 	private int pris;
 	private Spiller ejer;
 	private int pant;
-	private Color farve;
 	
-	public AbstraktEjendom(String beskrivelse, String subText, String titel, int pris, Spiller ejer, int pant,
-			Color farve) {
+	public AbstraktEjendom(String beskrivelse, String subText, String titel, int pris, 
+			Spiller ejer, int pant) {
 		super(beskrivelse, subText, titel);
 		this.pris = pris;
 		this.ejer = ejer;
 		this.pant = pant;
-		this.farve = farve;
 	}
 	public void setPant(int pant) {
 		this.pant = pant;
--- src/main/java/gr_34/entity/Felt.java
+++ src/main/java/gr_34/entity/Felt.java
@@ -1,6 +1,6 @@
 package gr_34.entity;
 
-public class Felt {
+public abstract class Felt {
 	
 	private String beskrivelse;
 	private String subText;
