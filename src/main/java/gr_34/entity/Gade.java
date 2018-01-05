3819efe32c202f993b61d24f8d00efdf6ba91186
Burhan27 <32563710+Burhan27@users.noreply.github.com>
2018-01-05 11:46:39 +0100

    opdateret gade og felt klasser

--- src/main/java/gr_34/entity/Gade.java
+++ src/main/java/gr_34/entity/Gade.java
@@ -1,5 +1,42 @@
 package gr_34.entity;
 
-public class Gade {
+import java.awt.Color;
+
+public class Gade extends Felt {
+
+	private int pris;
+	private Spiller ejer;
+	private int pant;
+	private Color farve;
+
+	public Gade(int pris, Spiller ejer, int pant, Color farve) {
+		this.pris = pris;
+		this.ejer = ejer;
+		this.pant = pant;
+		this.farve = farve;
+	}
+	public void setPant(int pant) {
+		this.pant = pant;
+	}
+	public int getPant() {
+		return this.pant;
+	}
+
+	public Color getFarve() {
+		return farve;
+	}
+
+	public Spiller getEjer() {
+		return this.ejer;
+	}
+	public void setEjer(Spiller ejer) {
+		this.ejer = ejer;
+	}
+	public int getPris() {
+		return this.pris;
+	}
+	public void setPris(int pris) {
+		this.pris = pris;
+	}
 
 }
