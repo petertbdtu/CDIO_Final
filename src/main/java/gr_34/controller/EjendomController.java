17f591a638453d8ec6e57fdf2e254c4aff3ce0f7
Burhan27 <32563710+Burhan27@users.noreply.github.com>
2018-01-08 14:48:53 +0100

    EjendomController kender gader, rederier og bryggerier

--- src/main/java/gr_34/controller/EjendomController.java
+++ src/main/java/gr_34/controller/EjendomController.java
@@ -2,6 +2,7 @@ package gr_34.controller;
 
 import gr_34.entity.Spiller;
 import gr_34.entity.felter.AbstraktEjendom;
+import gr_34.entity.felter.AbstraktFelt;
 import gr_34.entity.felter.Bryggeri;
 import gr_34.entity.felter.Gade;
 import gr_34.entity.felter.Rederi;
@@ -10,16 +11,44 @@ import gui_main.GUI;
 public class EjendomController {
 	
 	private GUI g;
-	private Gade[] gader;
-	private Bryggeri[] bryggerier;
-	private Rederi[] rederier;
+	private final int ANTAL_GADER = 22;
+	private final int ANTAL_BRYGGERIER = 2;
+	private final int ANTAL_REDERIER = 4;
+	private Gade[] gader = new Gade[ANTAL_GADER];
+	private Bryggeri[] bryggerier = new Bryggeri[ANTAL_BRYGGERIER];
+	private Rederi[] rederier = new Rederi[ANTAL_REDERIER];
 	
-	public EjendomController (GUI gui, Gade[] gader, Bryggeri[] bryggerier, Rederi[] rederier)
+	public EjendomController (GUI gui, BrætController bræt)
 	{
 		this.g = gui;
-		this.gader = gader;
-		this.bryggerier = bryggerier;
-		this.rederier = rederier;
+		
+		AbstraktFelt[] felter = bræt.getFelter();
+		
+		// TODO Find ud af om der er en bedre måde at finde alle slags ejendomme.
+		
+		int gadeTæller = 0;
+		int rederiTæller = 0;
+		int bryggeriTæller = 0;
+		
+		for (int i = 0; i < felter.length; i++)
+		{
+			if (felter[i] instanceof Gade)
+			{
+				gader[gadeTæller] = (Gade) felter[i];
+				gadeTæller++;
+			}
+			else if (felter[i] instanceof Rederi)
+			{
+				rederier[rederiTæller] = (Rederi) felter[i];
+				rederiTæller++;
+			}
+			else if (felter[i] instanceof Bryggeri)
+			{
+				bryggerier[bryggeriTæller] = (Bryggeri) felter[i];
+				bryggeriTæller++;
+			}
+		}
+		
 	}
 	
 	public void ramtEjendom(AbstraktEjendom ejendom, Spiller spiller)
