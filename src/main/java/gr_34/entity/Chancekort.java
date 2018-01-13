4a57de407cc3897e6f31f54bb8e3e40b3ad201f0
Alan Jafi <Alan@alanjafis-mbp.eduroam.wireless.dtu.dk>
2018-01-13 11:45:16 +0100

    chancekort creator oprettet

--- src/main/java/gr_34/entity/Chancekort.java
+++ src/main/java/gr_34/entity/Chancekort.java
@@ -1,20 +1,14 @@
 package gr_34.entity;
 
 public class Chancekort {
-	private String beskrivelse;
+	
+
+	
 	private ChanceEffekt effekt;
 	
-	public Chancekort(String beskrivelse, ChanceEffekt effekt) {
-		this.beskrivelse = beskrivelse;
+	public Chancekort(ChanceEffekt effekt) {
+		
 		this.effekt = effekt;
-	}
-
-	public String getBeskrivelse() {
-		return beskrivelse;
-	}
-
-	public void setBeskrivelse(String beskrivelse) {
-		this.beskrivelse = beskrivelse;
 	}
 
 	public ChanceEffekt getEffekt() {
